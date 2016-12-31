5f91013b39432e023ea60790b8f974e6fe118252
Plamen Lakov <frainbreeze3@gmail.com>
2016-12-31 18:01:01 +0200

    Find bug in 08

---
+++ Javascript Fundamentals/05. Conditional Statements/08NumberAsWords.js
@@ -0,0 +1,29 @@
+function solve(args) {
+    var num = args[0] | 0;
+    var arrOne = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+    var arrTwo = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    var arrThree = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    var result = '';
+
+    if(num == 0) {
+        result += 'zero';
+    }
+    else if(((num / 100) % 10) | 0 != 0) {
+        result += arrOne[((num / 100) % 10) | 0] + ' hundred ';
+        if(num % 100 > 0) {
+            result += 'and ';
+        }
+    }
+    num %= 100;
+    if(num >= 20 && num <= 99) {
+        result += arrThree[((num / 10) | 0) - 2];
+        result += ' ' + arrOne[num % 10];
+    }
+    else if(num >= 11 && num <= 19) {
+        result += arrTwo[(num % 10) - 1];
+    }else{
+        result += arrOne[num];
+    }
+    console.log(result.charAt(0).toUpperCase() + result.slice(1));
+}
+solve(['999']);
