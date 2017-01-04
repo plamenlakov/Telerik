f8984b470a6ccd5b536511e629d92bc7adde3ba2
Plamen Lakov <frainbreeze3@gmail.com>
2017-01-04 19:29:21 +0200

    Fixed bug

--- Javascript Fundamentals/06. Loops/02MinMaxSumAvg.js
+++ Javascript Fundamentals/06. Loops/02MinMaxSumAvg.js
@@ -1,10 +1,21 @@
 function solve(args) {
-    var a = args[0] | 0,
-        b = args[1] | 0,
-        c = args[2] | 0;
+    var nums = args.map(Number),
+        max = nums[0],
+        min = nums[0],
+        sum = nums[0];
+    
+    for(var i = 1; i < nums.length; i++) {
+        if(nums[i] > max) {
+            max = nums[i];
+        }
+        if(nums[i] < min) {
+            min = nums[i];
+        }
+        sum += nums[i];
+    }
 
-    console.log('min=' + Math.min(a, b, c).toFixed(2));
-    console.log('max=' + Math.max(a, b, c).toFixed(2));
-    console.log('sum=' + (a + b + c).toFixed(2));
-    console.log('avg=' + ((a + b + c) / 3).toFixed(2)); 
+    console.log('min=' + min.toFixed(2));
+    console.log('max=' + max.toFixed(2));
+    console.log('sum=' + sum.toFixed(2));
+    console.log('avg=' + (sum / nums.length).toFixed(2)); 
 }
